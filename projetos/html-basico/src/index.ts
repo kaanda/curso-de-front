c98d2e25f6d35ce65e7fd3f8208769f58d340443
Cleber Hensel <cleber.hensel@cwi.com.br>
2019-06-18 11:29:07 -0300

    Alterações para rodar a aplicação do projeto

--- projetos/html-basico/src/index.ts
+++ projetos/html-basico/src/index.ts
@@ -1,34 +1,43 @@
 import { Evento, IEvento } from './Evento';
+export class APP {
+    constructor() {
+        this.init();
+    }
 
-//lista dos eventos - criando tabela dinamicamente
-const objEvento: IEvento = new Evento();
+    //O que acontece quando a aplicação inicia???
+    init() {
+        //lista dos eventos - criando tabela dinamicamente
+        const objEvento: IEvento = new Evento();
 
-const tabela = document.getElementById("lista-de-eventos");
-const todosEventos = objEvento.getTodosEventos();
+        const tabela = document.getElementById("lista-de-eventos");
+        const todosEventos = objEvento.getTodosEventos();
 
-let linhas = ``;
+        let linhas = ``;
 
-// um foreach para criar as linhas da tabela, recebendo os valores
-todosEventos.forEach((evento: IEvento) => {
-    linhas += `<tr>
-                <td>${evento.titulo}</td>
-                <td>${evento.horario}</td>
-            </tr>`;
-});
+        // um foreach para criar as linhas da tabela, recebendo os valores
+        todosEventos.forEach((evento: IEvento) => {
+            linhas += `<tr>
+                        <td>${evento.titulo}</td>
+                        <td>${evento.horario}</td>
+                    </tr>`;
+        });
 
-//adicionando o conteúdo das linhas dinamicamente, sem precisar das refresh
-tabela.innerHTML = linhas;
+        //adicionando o conteúdo das linhas dinamicamente, sem precisar das refresh
+        tabela.innerHTML = linhas;
 
-//Captura do formulário
-const form = document.getElementById("form-adicionar-evento");
-console.log(form);
+        //Captura do formulário
+        const form = document.getElementById("form-adicionar-evento");
 
-//
-form.addEventListener("submit", Event => {
-    Event.preventDefault();
-    const titulo = document.getElementById("titulo_do_evento").nodeValue;
-    const horario = document.getElementById("horario_do_evento").nodeValue;
+        //
+        form.addEventListener("submit", Event => {
+            Event.preventDefault();
+            const titulo = document.getElementById("titulo_do_evento").nodeValue;
+            const horario = document.getElementById("horario_do_evento").nodeValue;
 
-    const evento = new Evento();
-    evento.adicionarNovoEvento(titulo, horario);
-});
+            const evento = new Evento();
+            evento.adicionarNovoEvento(titulo, horario);
+        });
+    }
+}
+
+window.addEventListener('load', () => new APP().init());
